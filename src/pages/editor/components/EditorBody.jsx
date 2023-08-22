4efdea5e840d074216147d33428ad2a921b166a0
john <johntehnician@gmail.com>
2023-08-22 23:54:29 +0900

    forth change url version

--- src/pages/editor/components/EditorBody.jsx
+++ src/pages/editor/components/EditorBody.jsx
@@ -228,7 +228,7 @@ function EditorBody({storeAt, index}) {
 
         var config = {
             method: "post",
-            url: "https://http://94.131.99.105:3000",
+            url: "http://94.131.99.105:3000",
             headers: {
                 "Content-Type": "application/json",
             },
